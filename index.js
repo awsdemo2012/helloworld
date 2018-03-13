261a953aeb39842ff00dc723f5d6a087df53c854
Raghav <awsdemo2012@gmail.com>
2018-03-12 20:39:40 -0700

    test docker build image from jenkins.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function onRequest(request, response) {
   response.write('Hello World');
   response.end();
 }
-
+// This will create a server on port 5000
 http.createServer(onRequest).listen(5000);
 
 console.log('Started server @ 5000');
